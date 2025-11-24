09b75d6f7618ec86c711c50ada46f4f6abd6f71e
Keshia Poedjiono <keshiapud@gmail.com>
2025-11-24 15:59:59 +0800

    recover history

--- src/routes/history.tsx
+++ src/routes/history.tsx
@@ -1,12 +1,1 @@
-import { WindowWrapper } from "@/components/global/window-wrapper";
-
-export const History = () => {
-  return (
-    <WindowWrapper title="FileBert - History">
-      {/* Main Content */}
-      <div className="flex-1 flex flex-col items-center justify-center px-6 py-8">
-        <p className="text-muted-foreground">History content coming soon...</p>
-      </div>
-    </WindowWrapper>
-  );
-};
+h
